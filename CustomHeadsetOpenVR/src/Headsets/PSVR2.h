f6a30326da803fe649f1c87ef7fca54712d3cbb2
Bela Kampis <belakampis@gmail.com>
2025-12-29 15:58:19 -0500

    Formatting

--- CustomHeadsetOpenVR/src/Headsets/PSVR2.h
+++ CustomHeadsetOpenVR/src/Headsets/PSVR2.h
@@ -8,11 +8,11 @@
 #include <cmath>
 #include <mutex>
 
-
-class PSVR2Shim : public ShimDefinition{
+class PSVR2Shim : public ShimDefinition
+{
 public:
 	CustomHeadsetDeviceProvider* deviceProvider = nullptr;
-	
+
 	DistortionProfileConstructor distortionProfileConstructor;
 	std::mutex distortionProfileLock;
 	// time that last frame occurred
@@ -31,31 +31,31 @@ public:
 	vr::HmdMatrix34_t lastMovementRotation = {};
 	// brightness multiplier from for dimming when the headset is not moving
 	double dimmingMultiplier = 1;
-	
-	
+
+
 	// test thread that toggles testToggle every 5 seconds to test things
 	bool testToggle = false;
 	bool isActive = false;
 	std::thread testThread;
-	
-	virtual void PosTrackedDeviceActivate(uint32_t &unObjectId, vr::EVRInitError &returnValue) override;
+
+	virtual void PosTrackedDeviceActivate(uint32_t& unObjectId, vr::EVRInitError& returnValue) override;
 	virtual void PosTrackedDeviceDeactivate() override;
-	virtual bool PreDisplayComponentGetProjectionRaw(vr::EVREye &eEye, float *&pfLeft, float *&pfRight, float *&pfBottom, float *&pfTop) override;
-	virtual bool PreDisplayComponentComputeDistortion(vr::EVREye &eEye, float &fU, float &fV, vr::DistortionCoordinates_t &coordinates) override;
-	virtual bool PreDisplayComponentIsDisplayOnDesktop(bool &returnValue) override;
-	virtual bool PreDisplayComponentIsDisplayRealDisplay(bool &returnValue) override;
-	virtual bool PreDisplayComponentGetWindowBounds(int32_t *&pnX, int32_t *&pnY, uint32_t *&pnWidth, uint32_t *&pnHeight) override;
-	virtual bool PreDisplayComponentGetEyeOutputViewport(vr::EVREye &eEye, uint32_t *&pnX, uint32_t *&pnY, uint32_t *&pnWidth, uint32_t *&pnHeight) override;
-	virtual bool PreDisplayComponentGetRecommendedRenderTargetSize(uint32_t* &pnWidth, uint32_t* &pnHeight) override;
-	
+	virtual bool PreDisplayComponentGetProjectionRaw(vr::EVREye& eEye, float*& pfLeft, float*& pfRight, float*& pfBottom, float*& pfTop) override;
+	virtual bool PreDisplayComponentComputeDistortion(vr::EVREye& eEye, float& fU, float& fV, vr::DistortionCoordinates_t& coordinates) override;
+	virtual bool PreDisplayComponentIsDisplayOnDesktop(bool& returnValue) override;
+	virtual bool PreDisplayComponentIsDisplayRealDisplay(bool& returnValue) override;
+	virtual bool PreDisplayComponentGetWindowBounds(int32_t*& pnX, int32_t*& pnY, uint32_t*& pnWidth, uint32_t*& pnHeight) override;
+	virtual bool PreDisplayComponentGetEyeOutputViewport(vr::EVREye& eEye, uint32_t*& pnX, uint32_t*& pnY, uint32_t*& pnWidth, uint32_t*& pnHeight) override;
+	virtual bool PreDisplayComponentGetRecommendedRenderTargetSize(uint32_t*& pnWidth, uint32_t*& pnHeight) override;
+
 	// helper function to calculate the 100% render resolution
 	void GetRecommendedRenderTargetSize(uint32_t* renderWidth, uint32_t* renderHeight);
-	
+
 	void SetIPD(float ipd, float angle = 0);
-	
+
 	virtual void RunFrame() override;
-	
+
 	void UpdateSettings();
-	
+
 	void TestThread();
 };
